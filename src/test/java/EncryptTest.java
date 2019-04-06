cf92dbd3c80322f9e4b1c264660ef863a60cf158
abbiemurugi <abigaelmurugi@gmail.com>
2019-04-07 00:14:17 +0300

    declared an encrypt class and created a test method

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,5 +1,12 @@
 import static org.junit.Assert.*;
+import org.junit.*;
+
 
 public class EncryptTest {
+    @Test
+    public void isEncrypt_ableTo_encrypt() {
+        Encrypt encrypt = new Encrypt();
+        assertEquals("ab", encrypt.isEncrypt("ab",3));
 
+    }
 }
