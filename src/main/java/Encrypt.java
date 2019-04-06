07f4967e825a0e69b5d6464f3d03a3a929462e50
abbiemurugi <abigaelmurugi@gmail.com>
2019-04-07 00:12:06 +0300

    declared a class so as to compile

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
 
 public class Encrypt {
-}
+    public static String isEncrypt(String text, int shift) {
+        String result = new String();
+
+        for (int i=0; i<text.length(); i++);
+
+        return result;
+
+       }
+    }
+
+
+
+//        if(Character.isUpperCase(text.charAt(i)))
+//            char character=(\+
+//                shift - 65) % 26 + 65);
+//output.append();
