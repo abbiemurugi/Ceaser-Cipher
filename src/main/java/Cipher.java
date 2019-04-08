705442050a99391f897789935921f5dca702578e
abbiemurugi <abigaelmurugi@gmail.com>
2019-04-08 15:48:44 +0300

    creating a new class for getter and setter

---
+++ src/main/java/Cipher.java
@@ -0,0 +1,13 @@
+class Cipher{
+
+    private String mEncrypt;
+    private String mDecrypt;
+    private int mKey;
+    public Cipher(String encrypt, String decrypt, int key){
+        mEncrypt = encrypt;
+        mDecrypt = decrypt;
+        mKey = key;
+
+        }
+
+}
